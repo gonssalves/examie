df6740c1e94963434cb2382e3ba54164a8dbce57
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-04-30 14:25:13 -0300

    feat: change display of special method __repr__

--- models/entities.py
+++ models/entities.py
@@ -5,15 +5,15 @@ from app import bcrypt
 class Role(db.Model):
     #create the role table
     #define the users types
-    #TODO: User this table for authentication purposes
+    #TODO: Use this table for authentication purposes
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True, nullable=False)
     users = db.relationship('User', backref='role') #TODO: Search about the relationships
 
-    #print role name
+    #define how Role.query.all() will be print
     def __repr__(self):
-        return f'<Role {self.name}>'
+        return f'<ID: {self.id} - Role: {self.name} - Users: {self.users}>'
 
 class User(db.Model, UserMixin):
     #create the user table
@@ -26,9 +26,7 @@ class User(db.Model, UserMixin):
     
     def verify_password(self, password):
         ''' Use hashing to verify if the password passed through form is correct'''
-        #pw_hash = bcrypt.generate_password_hash(password)
         return bcrypt.check_password_hash(self.password, password)       
     
-    #print user name
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f'<ID: {self.id} - Username: {self.username}>'
