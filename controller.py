a3102995cfb67fc9e303fc64142195070d7ce3ae
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-04-30 20:58:31 -0300

    feat: move form classes and prepare signup view

--- controller.py
+++ controller.py
@@ -1,9 +1,6 @@
 from flask import render_template, redirect, url_for, session
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SelectField, SubmitField
-from wtforms.validators import InputRequired
-from flask import Blueprint
-from flask_login import login_required, login_user
+from flask import Blueprint, request
+from flask_login import login_required
 
 #HERE THE VIEWS ARE CREATED
 #VIEWS ARE FUNCTIONS RESPONSIBLE FOR HANDLING REQUESTS
@@ -11,21 +8,6 @@ from flask_login import login_required, login_user
 
 #create blueprint
 controller = Blueprint('controller', __name__)
-
-class LoginForm(FlaskForm):
-    #Form is sent to template through Jinja2. Some front-end validations
-    username = StringField(validators=[InputRequired()])
-    password = PasswordField(validators=[InputRequired()])
-    submit = SubmitField('Submit')
-
-class SignupForm(FlaskForm):
-    username = StringField(validators=[InputRequired()])
-    email = StringField(validators=[InputRequired()])
-    password = PasswordField(validators=[InputRequired()])
-    re_password = PasswordField('Re-enter Password', validators=[InputRequired()])
-    choices = [('2', 'Student'), ('3', 'Teacher'), ('1', 'Administrator')]
-    role = SelectField('Select User Role', choices=choices)
-    submit = SubmitField()
 
 #custom page for client-side error
 @controller.errorhandler(404)
@@ -39,34 +21,38 @@ def internal_server_error(e):
 
 @controller.route('/')
 def index():
+    ''' Verify if there's any user information stored in session, if so, redirect for login page. '''
+    
     #by default, Flask-Login uses sessions for authentication
-    #verify if there's any user information stored in session
     #session is a dictionary that the application uses to store values that are "remembered" beetween requests
 
-    session.clear()
+    #session.clear()
     
     #_user_id is added to session if the user is authenticated through login_user() 
     if '_user_id' in session:
-        return redirect(url_for('controller.signup'))
+        return render_template('index.html')
     else:
         return redirect(url_for('controller.login'))
 
-#instantiates the form class and invoke function responsible for login validation
-#see that I'm not using request.methods explicitly TODO: Check what validate_on_submit returns (proly just a boolean)
 @controller.route('/login', methods=['GET', 'POST'])
 def login():
+    ''' Login validation. '''
+    from forms import LoginForm
     form = LoginForm()
-    if form.validate_on_submit():
-        from models.forms import form_login
-        return form_login()
+    if form.validate_on_submit(): #see that I'm not using request.methods explicitly  
+        from models.auth import auth_login
+        return auth_login()
+    form.process(request.args)
     return render_template('login.html', form=form)
-
-#TODO: Exclude flask-bootstrap from application and create the other routes
+        
 @controller.route('/signup', methods=['GET', 'POST'])
 @login_required
 def signup():
+    ''' Check if the form is submitted. '''
+    from forms import SignupForm
     form = SignupForm()
     if form.validate_on_submit():
-        from models.forms import form_signup
-        return form_signup()
+        from models.auth import auth_signup
+        return auth_signup() 
+    form.process(request.args)#insert the request parameter in the form
     return render_template('signup.html', form=form)
