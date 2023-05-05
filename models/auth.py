dac6aad5cec141ea232996b9b04b30d42c1bc570
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-05-05 11:43:27 -0300

    feat: add email sender | fix: remember-me

--- models/auth.py
+++ models/auth.py
@@ -1,14 +1,18 @@
 from models.entities import User, Role
-from flask import request, redirect, url_for, flash
-from flask_login import login_user  
-from app import db, bcrypt
+from flask import request, redirect, url_for, flash, session
+from flask_login import login_user
+from app import db, bcrypt, mail
 from email_validator import validate_email, EmailNotValidError
+from email.message import EmailMessage
+import smtplib
+import ssl
+
 
 def auth_login():
     ''' Check if the user exists in the database and if the information submitted is correct.'''
     username = request.form.get('username')
     password = request.form.get('password')
-    remember_me = request.form.get('remember_me')
+    remember_me = (True if request.form.get('remember_me') else False)
     
     #check if user is on the database
     user = User.query.filter_by(username=username).first()#returns none if there's no such user
@@ -20,16 +24,56 @@ def auth_login():
     req.pop('submit')
 
     if user:
-        if user.verify_password(password):
-            login_user(user, remember_me)#once user is authenticated, he is logged with this function | remember-me can keep the user logged after the browser is closed
+        if user.verify_password(password) or password == user.password:
+            login_user(user, remember=remember_me)#once user is authenticated, he is logged with this function | remember-me can keep the user logged after the browser is closed
             return redirect(request.args.get('next') or url_for('main.index'))
     flash('Invalid username or password.')
     return redirect(url_for('auth.login', **req))#**req is used to sent the request to the form, so the user doesn't need to type everything again
     #TODO: search about this url_for parameters
 
 def auth_recovery():#TODO: search how to send email, gmail blocked some shit about "less safe apps" 
-    flash('Feature not implemented yet, probably will never be.')
-    return redirect(url_for('auth.login'))
+    email = request.form.get('email')
+
+    req = request.form.copy()
+    req.pop('csrf_token')
+    req.pop('submit')
+
+    try:
+        emailinfo = validate_email(email, check_deliverability=True)#check_deliverability DNS queries are made to check that the domain name in the email address (the part after the @-sign) can receive mail
+        email = emailinfo.normalized#
+    except EmailNotValidError as e:
+        flash(str(e))
+        return redirect(url_for('auth.account_recovery', **req))
+    
+    user = User.query.filter_by(email=email).first()
+
+    if user:
+        password = user.password
+        #Define email sender and receiver
+        email_sender = 'contacttrepverter@gmail.com'
+        email_password = 'ipxtsyzonfvriiem'
+        email_receiver = email
+
+        #Set the subject and body of the email
+        subject = 'Recovery Account!'
+        body = f'Use this password to log in: {password}'
+
+        em = EmailMessage()
+        em['From'] = email_sender
+        em['To'] = email_receiver
+        em['Subject'] = subject
+        em.set_content(body)
+        
+        # Add SSL (layer of security)
+        context = ssl.create_default_context()
+
+        # Log in and send the email
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+            smtp.login(email_sender, email_password)
+            smtp.sendmail(email_sender, email_receiver, em.as_string())
+
+    flash('If this email is registered, you received a message. Check your spam box.')
+    return redirect(url_for('auth.account_recovery'))
 
 def auth_signup():
     ''' Validate the informations sent and register an user. '''
