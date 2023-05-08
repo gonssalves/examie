a074bc547039035d6044fece2105496189705829
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-05-07 22:05:02 -0300

    feat: add function for password reset

--- models/auth.py
+++ models/auth.py
@@ -1,12 +1,11 @@
 from models.entities import User, Role
 from flask import request, redirect, url_for, flash, session
-from flask_login import login_user
+from flask_login import login_user, current_user
 from app import db, bcrypt, mail
 from email_validator import validate_email, EmailNotValidError
 from email.message import EmailMessage
 import smtplib
 import ssl
-
 
 def auth_login():
     ''' Check if the user exists in the database and if the information submitted is correct.'''
@@ -25,13 +24,37 @@ def auth_login():
 
     if user:
         if user.verify_password(password) or password == user.password:
+            if user.first_access:
+                login_user(user, remember=remember_me)
+                return redirect(url_for('auth.password_reset'))
             login_user(user, remember=remember_me)#once user is authenticated, he is logged with this function | remember-me can keep the user logged after the browser is closed
             return redirect(request.args.get('next') or url_for('main.index'))
     flash('Invalid username or password.')
     return redirect(url_for('auth.login', **req))#**req is used to sent the request to the form, so the user doesn't need to type everything again
     #TODO: search about this url_for parameters
 
-def auth_recovery():#TODO: search how to send email, gmail blocked some shit about "less safe apps" 
+def auth_reset():
+    password = request.form.get('password')
+    password2 = request.form.get('password2')
+
+    if password != password2:
+        flash('Passwords must be the same')
+        return redirect(url_for('auth.password_reset'))
+    
+    user = current_user
+
+    try:
+        user.password = bcrypt.generate_password_hash(password).decode('utf-8')
+        user.first_access = False
+        db.session.commit()
+    except:
+        flash('Unable to alter password, please try again later')
+        return redirect(url_for('auth.password_reset'))
+    
+    flash('Password changed')
+    return redirect(url_for('main.index'))
+    
+def auth_recovery():
     email = request.form.get('email')
 
     req = request.form.copy()
@@ -80,7 +103,7 @@ def auth_signup():
     username = request.form.get('username')
     email = request.form.get('email')
     password = request.form.get('password')
-    re_password = request.form.get('re_password')
+    password2 = request.form.get('password2')
     role = request.form.get('role')
 
     req = request.form.copy()
@@ -96,7 +119,7 @@ def auth_signup():
         flash(str(e))
         return redirect(url_for('auth.signup', **req))
 
-    if password != re_password:
+    if password != password2:
         flash('Passwords must be the same')
         return redirect(url_for('auth.signup', **req))
     
