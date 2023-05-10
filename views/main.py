0291a3192c4b680a42915acd9a63b285273e89af
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-05-10 17:25:48 -0300

    feat: add edit and exclude users

--- views/main.py
+++ views/main.py
@@ -30,7 +30,42 @@ from login_required import admin_login_required
 @admin_login_required()
 def users():
     from models.entities import User
+    from forms import SignupForm
+
     all_users = User.show_all()
-    return render_template('users.html', users=all_users)
+    form = SignupForm()
+
+    if form.validate_on_submit():
+        from models.auth import auth_signup
+        return auth_signup() 
+    return render_template('users.html', users=all_users, form=form)
+
+@main.route('/users/<int:user_id>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+@login_required
+@admin_login_required()
+def edit_users(user_id):
+    from models.entities import User
+    from forms import SignupForm
+
+    user = User.query.filter_by(id=user_id).first()
+    form = SignupForm(obj=user)
+
+    if form.validate_on_submit():
+        from models.auth import auth_edit
+        return auth_edit(user)
+    return render_template('edit_users.html', form=form)
+
+@main.route('/users/<int:user_id>/delete', methods=['GET', 'POST', 'PUT', 'DELETE'])
+@login_required
+@admin_login_required()
+def delete_users(user_id):
+    from models.entities import User
+
+    user = User.query.filter_by(id=user_id).first()
+  
+    from models.auth import auth_delete
+    return auth_delete(user)
+
+
 
 
