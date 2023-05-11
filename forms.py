666f159d3852fedeca2370ad55b90f9c7ab1fc85
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-05-11 13:47:29 -0300

    feat: add exam form

--- forms.py
+++ forms.py
@@ -51,7 +51,7 @@ class SignupForm(FlaskForm):
 class QuestionForm(FlaskForm):
     level_choices = [('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', ('5'))]
     answer_type_choices = [('op', 'Open Question'), ('mc', 'Multiple Choice'), ('sc', 'Single Choice'), ('tf', 'True or False')]
-    answer_choices = [('Answer2', 'Answer1'), ('Answer2', 'Answer2'), ('Answer3', 'Answer3'), ('Answer4', 'Answer4')]
+    answer_choices = [('Answer1', 'Answer1'), ('Answer2', 'Answer2'), ('Answer3', 'Answer3'), ('Answer4', 'Answer4')]
     true_or_false_choices = [('True', 'True'), ('False', 'False')]
 
     subject = StringField(validators=[InputRequired()])
@@ -60,14 +60,20 @@ class QuestionForm(FlaskForm):
     level = SelectField('Select the Question Level', choices=level_choices)
     tags = StringField(validators=[InputRequired()])
     answer_type = SelectField('Select the Question Type', choices=answer_type_choices)
-    answer1 = StringField(validators=[InputRequired()])
-    answer2 = StringField(validators=[InputRequired()])
-    answer3 = StringField(validators=[InputRequired()])
-    answer4 = StringField(validators=[InputRequired()])
-    single_right_answer = RadioField('Select the Right Answer', validators=[InputRequired()],choices=answer_choices)
-    true_or_false_answer = RadioField('Is the Answer', validators=[InputRequired()],choices=true_or_false_choices)
+    answer1 = StringField()
+    answer2 = StringField()
+    answer3 = StringField()
+    answer4 = StringField()
+    single_right_answer = RadioField('Select the Right Answer', choices=answer_choices)
+    true_or_false_answer = RadioField('Is the Answer', choices=true_or_false_choices)
     multiple_right_answer1 = BooleanField('Answer1')
     multiple_right_answer2 = BooleanField('Answer2')
     multiple_right_answer3 = BooleanField('Answer3')
     multiple_right_answer4 = BooleanField('Answer4')
-    submit = SubmitField()
+    submit = SubmitField('Submit')
+
+class ExamForm(FlaskForm):
+    opening_time = StringField('Opening Time', validators=[InputRequired()])
+    closing_time = StringField('Closing Time', validators=[InputRequired()])
+    execution_time = StringField('Execution Time', validators=[InputRequired()])
+    submit = SubmitField('Submit')
