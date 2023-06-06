ace8ce463d44df6643d19e49baaa94f90589ee10
Vinícius Gonçalves <vgo1@aluno.ifal.edu.br>
2023-06-06 16:43:21 -0300

    fix: add default error pages for erros 404 and 500

--- views/error.py
+++ views/error.py
@@ -3,11 +3,11 @@ from flask import Blueprint, render_template
 error = Blueprint('error', __name__)
 
 #custom page for client-side error
-@error.errorhandler(404)
+@error.app_errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
 #custom page for server-side error
-@error.errorhandler(500)
+@error.app_errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
